fd7e153c7359fdf7726bb9298e8cc0eda387e730
usertre <1623562970@qq.com>
2023-12-01 20:07:40 +0800

    2023/12/1
    将之前的一些内容提交上来，作为这个Project的开始
    Submit some of the previous content as the beginning of this project
    
    usertre(Wtpal)

---
+++ ConsoleApplication1/Include/Guns.h
@@ -0,0 +1,48 @@
+#ifndef GUN_H
+#define GUN_H
+#include <Number.h>
+
+char Guns_Name[GUNS_NUMBER][50] = {
+	"\"Dusk\" ce.III",
+	"Booker 风暴步枪 Model 838",
+	"Aalto 风暴步枪 24",
+	"7.92mm",
+	"Catara mo.II",
+	"KRN886-127 Gast Machine Gun",
+	"Malone Mk.2",
+	"Bomastone 手榴弹",
+	"A3 Harpa 破片手雷",
+	"Ferro 879",
+	"Cascadier 873",
+	"Ahti Model 2",
+	"8mm",
+	"Cometa T2-9",
+	"Hangman 757式步枪",
+	".44",
+	"Catena rt.IV 自动步枪",
+	"Sampo 自动步枪 77",
+	"Argenti r.II 步枪",
+	"Volta r.I Repeater",
+	"Fuscina pi.I",
+	"Blakerow 871",
+	"KRR2-790 Omen",
+	"Clancy Cinder M3",
+	"No.2B Hawthorne",
+	"No.2 Loughcaster",
+	"KRR3-792 Auger",
+	"Clancy-Raca M4",
+	"7.62mm",
+	"Brasa 霰弹枪",
+	"散弹",
+	"\"The Pitch Gun\" mc.V",
+	"\"Lionclaw\" mc.VIII",
+	"No.1 \"The Liar\" 冲锋枪",
+	"Fiddler Model 868冲锋枪",
+	"9mm",
+	"PT-815 烟雾弹",
+	"Green Ash手雷",
+	"12.7mm"
+};
+
+
+#endif // !GUN_H
---
+++ ConsoleApplication1/Include/Number.h
@@ -0,0 +1,19 @@
+#ifndef NUMBER_H
+#define NUMBER_H
+
+/*
+    仓库中每一页物品的种类数
+*/
+#define GUNS_NUMBER           39
+#define HEAVY_WEAPONS_NUMBER  30
+#define SHELL_NUMBER          16
+#define TOOLS_NUMBER          25
+#define MEDIC_NUMBER          5
+#define MATERIAL_NUMBER       27
+#define UNIFORM_NUMBER        19
+#define VEHICLE_CRATE_NUMBER  51
+#define VEHICLE_NUMBER        102
+#define OTHER_CRATE_NUMBER    12
+#define OTHER_NUMBER          21
+
+#endif // !NUMBER_H
